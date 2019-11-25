a0c18939a068ff5c7a2bc821e584065bc33c339f
Jorge Sanchez-Cano <jorgescmsd@gmail.com>
2019-11-25 10:04:06 +0000

    completed the UrbanSound8KDataset object. It can now be used to load the features.

--- dataset.py
+++ dataset.py
@@ -10,22 +10,40 @@ class UrbanSound8KDataset(data.Dataset):
         self.mode = mode
 
     def __getitem__(self, index):
+        item = self.dataset[index]
+        item_features = item['features']
+        cst = self.getCSTfeature(item_features)
         if self.mode == 'LMC':
-            # Edit here to load and concatenate the neccessary features to 
+            # Edit here to load and concatenate the neccessary features to
             # create the LMC feature
+            lm = item_features['logmelspec']
+            feature = np.concatenate((lm,cst))
             feature = torch.from_numpy(feature.astype(np.float32)).unsqueeze(0)
         elif self.mode == 'MC':
-            # Edit here to load and concatenate the neccessary features to 
+            # Edit here to load and concatenate the neccessary features to
             # create the MC feature
+            mfcc = item_features['mfcc']
+            feature = np.concatenate((mfcc,cst))
             feature = torch.from_numpy(feature.astype(np.float32)).unsqueeze(0)
         elif self.mode == 'MLMC':
-            # Edit here to load and concatenate the neccessary features to 
+            # Edit here to load and concatenate the neccessary features to
             # create the MLMC feature
+            mfcc = item_features['mfcc']
+            lm = item_features['logmelspec']
+            feature = np.concatenate((mfcc,lm,cst))
             feature = torch.from_numpy(feature.astype(np.float32)).unsqueeze(0)
-       
-        label = self.dataset[index]['classID']
-        fname = self.dataset[index]['filename']
+        else:
+            raise ValueError('Illegal Argument: mode must be one of {LMC,MC,MLMC},' +
+                            ' not ' + self.mode)
+
+        label = item['classID']
+        fname = item['filename']
         return feature, label, fname
+
+    def getCSTfeature(self, features):
+        return np.concatenate((features['chroma'],
+                               features['spectral_contrast'],
+                               features['tonnetz']))
 
     def __len__(self):
         return len(self.dataset)
