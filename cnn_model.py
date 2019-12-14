Changed the optimizer to adam and took out the batch normalization, which I think was wrong.

cnn_model.py
@@ -27,7 +27,7 @@ class CNN(nn.Module):
             padding=(1, 1),
         )
         self.initialise_layer(self.conv1)
-        self.conv1_bn = nn.BatchNorm2d(32)
+        #self.conv1_bn = nn.BatchNorm2d(32)
 
         #Second convolutional layer
         self.conv2 = nn.Conv2d(
@@ -38,7 +38,7 @@ class CNN(nn.Module):
             padding=(1, 1)
         )
         self.initialise_layer(self.conv2)
-        self.conv2_bn = nn.BatchNorm2d(32)
+        #self.conv2_bn = nn.BatchNorm2d(32)
 
         #Max pooling
         self.pool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2), padding=(1,1))
@@ -52,7 +52,7 @@ class CNN(nn.Module):
             padding=(1, 1),
         )
         self.initialise_layer(self.conv1)
-        self.conv3_bn = nn.BatchNorm2d(64)
+        #self.conv3_bn = nn.BatchNorm2d(64)
 
         #Fourth convolutional layer
         self.conv4 = nn.Conv2d(
@@ -63,7 +63,7 @@ class CNN(nn.Module):
             padding=(1, 1)
         )
         self.initialise_layer(self.conv2)
-        self.conv4_bn = nn.BatchNorm2d(64)
+        #self.conv4_bn = nn.BatchNorm2d(64)
 
         #Max pooling
         self.pool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2), padding=(1,1))
@@ -71,7 +71,7 @@ class CNN(nn.Module):
         #First fully connected layer
         self.fc1 = nn.Linear(15488, 1024)
         self.initialise_layer(self.fc1)
-        self.fc1_bn = nn.BatchNorm1d(1024)
+        #self.fc1_bn = nn.BatchNorm1d(1024)
 
         #Final fully connected layer
         self.fc2 = nn.Linear(1024, 10)
@@ -79,14 +79,14 @@ class CNN(nn.Module):
 
     def forward(self, images: torch.Tensor) -> torch.Tensor:
         #Perform the forward pass through the network
-        x = F.relu(self.conv1_bn(self.conv1(images)))
-        x = self.dropout2d(F.relu(self.conv2_bn(self.conv2(x))))
+        x = F.relu(self.conv1(images))
+        x = self.dropout2d(F.relu(self.conv2(x)))
         x = self.pool1(x)
-        x = F.relu(self.conv3_bn(self.conv3(x)))
-        x = self.dropout2d(F.relu(self.conv4_bn(self.conv4(x))))
+        x = F.relu(self.conv3(x))
+        x = self.dropout2d(F.relu(self.conv4(x)))
         x = self.pool2(x)
         x = torch.flatten(x, start_dim=1)
-        x = self.dropout(torch.sigmoid(self.fc1_bn(self.fc1(x))))
+        x = self.dropout(torch.sigmoid(self.fc1(x)))
         x = self.fc2(x)
         return x
 

group_cnn.py
@@ -119,8 +119,8 @@ def main(args):
     ## TASK 8: Redefine the criterion to be softmax cross entropy
     criterion = nn.CrossEntropyLoss()
 
-    ## TASK 11: Define the optimizer
-    optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=args.sgd_momentum)
+    ## Use adam optimizer. Weight decay indicates L-2 regularization is applied to weights.
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=1)
 
     log_dir = get_summary_writer_log_dir(args)
     print(f"Writing logs to {log_dir}")
@@ -309,6 +309,15 @@ def compute_accuracy(
     assert len(labels) == len(preds)
     return float((labels == preds).sum()) / len(labels)
 
+# def per_class_accuracy(labels, preds, number_classes):
+#     """ Calculate the accuracy for each class. """
+#     correct = [x == y for x,y in zip(labels,preds)]
+#     accuracies = np.zeros(number_classes)
+#     # Assuming there is the same number of examples per class:
+#     N = len(labels)/number_classes
+#     for index,class in enumerate(labels):
+#         accuracies[class] += correct[index]
+#     return accuracies/N
 
 def get_summary_writer_log_dir(args: argparse.Namespace) -> str:
     """Get a unique directory that hasn't been logged to before for use with a TB